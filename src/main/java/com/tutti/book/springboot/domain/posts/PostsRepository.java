bcdd30ec6d3bd68d513f058a1ea876841b93a8df
JongMinChoi <wonderland314592@gmail.com>
2022-05-04 01:06:37 +0900

    add PostsRepository, Posts, implementation h2 db,spring-boot-starter-data-jpa

---
+++ src/main/java/com/tutti/book/springboot/domain/posts/PostsRepository.java
@@ -0,0 +1,6 @@
+package com.tutti.book.springboot.domain.posts;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface PostsRepository extends JpaRepository<Posts,Long> { // Dao < Entity class, PK type >
+}
