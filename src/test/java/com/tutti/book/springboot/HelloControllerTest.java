4779749c59ad559a58568f0810c0968b89d801f3
JongMinChoi <wonderland314592@gmail.com>
2022-05-03 23:40:31 +0900

    TDD, unit test code, add lombok

---
+++ src/test/java/com/tutti/book/springboot/HelloControllerTest.java
@@ -0,0 +1,87 @@
+package com.tutti.book.springboot;
+
+import com.tutti.book.springboot.web.HelloController;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.
+        Autowired;
+import org.springframework.boot.test.autoconfigure.web.
+        servlet.WebMvcTest;
+import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.ResultActions;
+
+
+import static org.hamcrest.Matchers.is;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+@RunWith(SpringRunner.class)
+/*
+    # 테스트를 진행할 때 Junit에 내장된 실행자 외에 다른 실행자를 실행시킵니다.
+    # 여기서는 SpringRunner라는 스프링 실행자를 사용합니다.
+    # 즉, 스프링 부트 테스트와 Junit 사이에 연결자 역활을 합니다.
+ */
+@WebMvcTest(controllers = HelloController.class)
+/*
+    # 여러 스프링 테스트 어노테이션 중, Web(Spring MVC)에 집중할 수 있는 어노테이션입니다.
+    # 선언할 경우 @Controller, @ControllerAdvice 등을 사용할 수 있습니다.
+    # 단 @Service, @Component, @Repository 등은 사용할 수 없습니다.
+    # 여기서는 컨트롤러만 사용하기 때문에 선언합니다.
+ */
+
+public class HelloControllerTest {
+    @Autowired                        // 스프링이 관리하는 빈(bean)을 주입 받습니다.
+    private MockMvc mvc;
+    /*
+        웹 API를 테스트할 때 사용합니다.
+        스프링 MVC 테스트의 시작점입니다.
+        이 클래스를 통해 HTTP GET, POST, 등에 대한 API 테스트를 할 수 있습니다.
+     */
+    @Test
+    public void hello가_리턴된다() throws Exception {
+        String hello = "hello";
+
+        mvc.perform(get("/hello"))
+                .andExpect(status().isOk())
+                /*
+                    mvc.perform의 결과를 검증합니다.
+                    HTTP Header의 Status를 검증합니다.
+                    200(OK, 클라이언트의 요청을 성공적으로 처리), 404(요청 리소스를 찾을 수 없을 떄), 500(서버오류) 등의 상태를 검증합니다.
+                    여기선 OK 즉, 200인지 아닌지를 검증합니다.
+                 */
+                .andExpect(content().string(hello));
+                /*
+                    mvc.perform의 결과를 검증합니다.
+                    응답 본문의 내용을 검증합니다.
+                    Cotroller에서 "hello"를 리턴하기 떄문에 이 값이 맞는지 검증합니다.
+                 */
+    }
+
+    @Test
+    public void helloDto가_리턴된다() throws Exception {
+        String name = "hello";
+        int amount = 1000;
+
+        mvc.perform(
+                    get("/hello/dto")
+                            .param("name", name)
+                            /*
+                                API 테스트 할 때 사용될 요청 파라미터를 설정합니다.
+                                단 값은 String만 허용됩니다.
+                                그래서 숫자/날짜 등의 데이터도 등록할 때는 문자열로 변경해야만 가능합니다.
+                             */
+                            .param("amount", String.
+valueOf(amount)))
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$.name", is(name)))
+                /*
+                    JSON 응답값을 필드별로 검증할 수 있는 메소드입니다.
+                    $를 기준으로 필드명을 명시합니다.
+                    여기서는 name과 amount를 검증하니 $.name, $.amount로 검증합니다.
+                 */
+                .andExpect(jsonPath("$.amount", is(amount)));
+    }
+}
